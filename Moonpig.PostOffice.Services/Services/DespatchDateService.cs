9e4f3523744f2a6d03b166bcc3a30d0b650edce8
hcansiz <huseyin_cansiz@hotmail.com>
2025-06-15 15:20:56 +0100

    Creates DbContext once in constructor instead of each method call

--- Moonpig.PostOffice.Services/Services/DespatchDateService.cs
+++ Moonpig.PostOffice.Services/Services/DespatchDateService.cs
@@ -8,20 +8,26 @@ namespace Moonpig.PostOffice.Services
 {
     public class DespatchDateService : IDespatchDateService
     {
+        private readonly DbContext _dbContext;
+
+        public DespatchDateService()
+        {
+            _dbContext = new DbContext();
+        }
+
         public DateTime CalculateDespatchDate(List<int> productIds, DateTime orderDate)
         {
             DateTime maxLeadTime = orderDate;
-            
+
             foreach (var productId in productIds)
             {
-                DbContext dbContext = new DbContext();
-                var supplierId = dbContext.Products.Single(x => x.ProductId == productId).SupplierId;
-                var leadTime = dbContext.Suppliers.Single(x => x.SupplierId == supplierId).LeadTime;
-                
+                var supplierId = _dbContext.Products.Single(x => x.ProductId == productId).SupplierId;
+                var leadTime = _dbContext.Suppliers.Single(x => x.SupplierId == supplierId).LeadTime;
+
                 if (orderDate.AddDays(leadTime) > maxLeadTime)
                     maxLeadTime = orderDate.AddDays(leadTime);
             }
-            
+
             if (maxLeadTime.DayOfWeek == DayOfWeek.Saturday)
             {
                 return maxLeadTime.AddDays(2);
